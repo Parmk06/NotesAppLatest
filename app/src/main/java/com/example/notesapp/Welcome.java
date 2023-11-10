e1fe784d223e79c5886f780f4ab8d639033dc1ab
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-09 22:30:19 -0500

    linked Login page to welcome

--- app/src/main/java/com/example/notesapp/Welcome.java
+++ app/src/main/java/com/example/notesapp/Welcome.java
@@ -2,7 +2,9 @@ package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        getSupportActionBar().setTitle("Welcome page");
+    }
+    public void openLoginPage(View view){
+        
+        startActivity(new Intent(this,Login.class));
+    }
+    public void openRegisterationPage(View view){
+
+        startActivity(new Intent(this,Register.class));
     }
 }
