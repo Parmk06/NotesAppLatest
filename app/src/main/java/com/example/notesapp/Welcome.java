bbe4f582fefc00a4843e09f6bbcbd7763ffafefa
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-09 22:33:39 -0500

    linked register to welcome page using Intent

--- app/src/main/java/com/example/notesapp/Welcome.java
+++ app/src/main/java/com/example/notesapp/Welcome.java
@@ -14,7 +14,11 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().setTitle("Welcome page");
     }
-    public void openLoginPage(View view){
+    public void openLoginPage(View view)
+    {
         startActivity(new Intent(this,Login.class));
+    }
+    public void openRegisterPage(View view){
+        startActivity(new Intent(this,Register.class));
     }
 }
